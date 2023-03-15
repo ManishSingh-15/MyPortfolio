d727b895e3f0cd9f487b14e2aec870bcc82286ed
ManishSingh-15 <manish.s15j@gmail.com>
2023-03-15 13:46:48 +0530

    Updates

---
+++ precache-manifest.ccf65ba7ee2b90741bda007880eb457b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0054010b43052c6dbe3",
+    "url": "/MyPortfolio/static/css/main.52e476ce.chunk.css"
+  },
+  {
+    "revision": "a0054010b43052c6dbe3",
+    "url": "/MyPortfolio/static/js/main.a0054010.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/MyPortfolio/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "d328f9c0f7cb3d523e0d",
+    "url": "/MyPortfolio/static/js/2.d328f9c0.chunk.js"
+  },
+  {
+    "revision": "5c74b59d45e91a2435de",
+    "url": "/MyPortfolio/static/js/runtime~main.5c74b59d.js"
+  },
+  {
+    "revision": "d7a25b0d172685bbbbec0e7babb84edc",
+    "url": "/MyPortfolio/index.html"
+  }
+];
